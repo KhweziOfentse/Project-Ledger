9687308639bf343b31c757532506bdc63afb9c0e
KhweziOfentse <158312796+KhweziOfentse@users.noreply.github.com>
2026-04-22 21:59:53 +0200

    added logic for navbar and side bar
    added logic for navbar and side bar

--- project-ledger/src/app/app.component.ts
+++ project-ledger/src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "./layout/sidebar/sidebar.component";
+import { NavbarComponent } from "./layout/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'project-ledger';
+
+  constructor(private router: Router) {}
+
+  isAuthPage(): boolean {
+    return ['/', '/login', '/register'].includes(this.router.url);
+  }
 }
